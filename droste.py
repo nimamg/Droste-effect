a7636d9b308028b1d99b589612f74f8eee906321
Nima <nima.mg@gmail.com>
2019-06-10 19:19:03 +0430

    rotation and exponential mappings done

--- droste.py
+++ droste.py
@@ -3,6 +3,7 @@ import numpy as np
 import copy as cp
 
 img = cv2.imread('clock.jpg', 1)
+# img = np.zeros((10,10,3))
 cv2.imshow('im',img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
@@ -32,31 +33,35 @@ wx = np.real(w)
 wy = np.imag(w)
 wxMax = np.max(abs(wx))
 wyMax = np.max(abs(wy))
-xNew = (wx / wxMax + 1) * len(img[0]) / 2
-yNew = (wy / wyMax + 1) * len(img) / 2
-np.clip(xNew,0,len(img) - 1,xNew)
-np.clip(yNew,0,len(img[0]) - 1,yNew)
-np.floor(xNew,xNew)
-np.floor(yNew,yNew)
-lgMapped = np.empty(img.shape,dtype='uint8')
-xNew = xNew.astype(int)
-yNew = yNew.astype(int)
+# xNew = (wx / wxMax + 1) * len(img[0]) / 2
+# yNew = (wy / wyMax + 1) * len(img) / 2
+# np.clip(xNew,0,len(img) - 1,xNew)
+# np.clip(yNew,0,len(img[0]) - 1,yNew)
+# np.floor(xNew,xNew)
+# np.floor(yNew,yNew)
+# lgMapped = np.empty(img.shape,dtype='uint8')
+# xNew = xNew.astype(int)
+# yNew = yNew.astype(int)
 
-for i in range(len(xNew)):
-    for j in range(len(yNew)):
-        lgMapped[yNew[i][j]][xNew[i][j]] = img[i][j]
-cv2.imshow('im',lgMapped)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# for i in range(len(xNew)):
+#     for j in range(len(yNew)):
+#         lgMapped[yNew[i][j]][xNew[i][j]] = img[i][j]
+# cv2.imshow('im',lgMapped)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
 
 # tiledPic = np.tile(lgMapped, (3,1,1))
-xNew = np.tile(xNew, (3,1))
-yAdd = cp.deepcopy(yNew)
-yAdd2 = cp.deepcopy(yNew)
-yAdd += 400
-yAdd2 += 800
-yNew = np.concatenate((yNew, yAdd))
+xNew = np.tile(wx, (3,1))
+yAdd = cp.deepcopy(wy)
+yAdd2 = cp.deepcopy(wy)
+yAdd += 2*np.max(abs(wy))
+yAdd2 += 4*np.max(abs(wy))
+print(np.around(wy, decimals=2))
+yNew = np.concatenate((wy, yAdd))
+print(np.around(yNew, decimals=2))
 yNew = np.concatenate((yNew, yAdd2))
+print(np.around(yNew, decimals=2))
+
 z = xNew + 1j * yNew
 # cv2.imshow('im',tiledPic)
 # cv2.waitKey(0)
@@ -76,23 +81,19 @@ rotY = np.imag(rz)
 rxMax = np.max(abs(rotX))
 ryMax = np.max(abs(rotY))
 xNew2 = (rotX / rxMax + 1) * 400 / 2
-yNew2 = (rotY / ryMax + 1) * 1200 / 2
+yNew2 = (rotY / ryMax + 1) * 400 / 2
 np.floor(xNew2,xNew2)
 np.floor(yNew2,yNew2)
-rotatedPic = np.zeros((1200,400,3),dtype='uint8')
+rotatedPic = np.zeros((400,400,3),dtype='uint8')
 xNew2 = xNew2.astype(int)
 yNew2 = yNew2.astype(int)
 # print(xNew2, len(tiledPic), len(tiledPic[0]), rotatedPic.shape, tiledPic.shape)
 np.clip(xNew2,0,400 - 1,xNew2)
-np.clip(yNew2,0,1200 - 1,yNew2)
+np.clip(yNew2,0,400 - 1,yNew2)
 # print("HHHHHHHHHHHHH")
 # print(len(xNew2), len(yNew2))
 for p in range(1200):
     for q in range(400):
-        if yNew[p][q] > 1200:
-            print("Y")
-        elif xNew[p][q] > 1200:
-            print("x")
         rotatedPic[yNew2[p][q]][xNew2[p][q]] = img[p%400][q]
 
 cv2.imshow('im',rotatedPic)
