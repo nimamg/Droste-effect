dcb403199eedc34c9f48bfadc43932e984a77f7b
Nima <nima.mg@gmail.com>
2019-06-14 23:37:47 +0430

    project completed

--- droste.py
+++ droste.py
@@ -3,11 +3,11 @@ import numpy as np
 import copy as cp
 
 img = cv2.imread('clock.jpg', 1)
-# img = np.zeros((10,10,3))
 cv2.imshow('im',img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
+# Part 1
 rows = np.linspace(start = -1, stop = 1, num = len(img[0]))
 cols = np.linspace(start = -1, stop = 1, num = len(img))
 x, y = np.meshgrid(cols, rows)
@@ -33,46 +33,69 @@ wx = np.real(w)
 wy = np.imag(w)
 wxMax = np.max(abs(wx))
 wyMax = np.max(abs(wy))
-# xNew = (wx / wxMax + 1) * len(img[0]) / 2
-# yNew = (wy / wyMax + 1) * len(img) / 2
-# np.clip(xNew,0,len(img) - 1,xNew)
-# np.clip(yNew,0,len(img[0]) - 1,yNew)
-# np.floor(xNew,xNew)
-# np.floor(yNew,yNew)
-# lgMapped = np.empty(img.shape,dtype='uint8')
-# xNew = xNew.astype(int)
-# yNew = yNew.astype(int)
+lgxNew = (wx / wxMax + 1) * len(img[0]) / 2
+lgyNew = (wy / wyMax + 1) * len(img) / 2
+np.clip(lgxNew, 0, len(img) - 1, lgxNew)
+np.clip(lgyNew, 0, len(img[0]) - 1, lgyNew)
+np.floor(lgxNew,lgxNew)
+np.floor(lgyNew,lgyNew)
+lgMapped = np.empty(img.shape,dtype='uint8')
+lgxNew = lgxNew.astype(int)
+lgyNew = lgyNew.astype(int)
 
-# for i in range(len(xNew)):
-#     for j in range(len(yNew)):
-#         lgMapped[yNew[i][j]][xNew[i][j]] = img[i][j]
-# cv2.imshow('im',lgMapped)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+for i in range(len(lgxNew)):
+    for j in range(len(lgyNew)):
+        lgMapped[lgyNew[i][j]][lgxNew[i][j]] = img[i][j]
+cv2.imshow('im',lgMapped)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+# End of Part 1
 
-# tiledPic = np.tile(lgMapped, (3,1,1))
-xNew = np.tile(wx, (3,1))
-yAdd = cp.deepcopy(wy)
-yAdd2 = cp.deepcopy(wy)
-yAdd += 2*np.max(abs(wy))
-yAdd2 += 4*np.max(abs(wy))
-print(np.around(wy, decimals=2))
-yNew = np.concatenate((wy, yAdd))
-print(np.around(yNew, decimals=2))
-yNew = np.concatenate((yNew, yAdd2))
-print(np.around(yNew, decimals=2))
+#Part 2
+rows = np.linspace(start = -1, stop = 1, num = len(img[0]))
+cols = np.linspace(start = -1, stop = 1, num = len(img))
+x, y = np.meshgrid(cols, rows)
+rotZ = x + 1j*y
+rotZ = z * np.exp(1j * np.pi / 4)
+rotX = np.real(rotZ)
+rotY = np.imag(rotZ)
+rotXmax = np.max(abs(rotX))
+rotYmax = np.max(abs(rotY))
+rotxNew = (rotX / rotXmax + 1) * len(img[0]) / 2
+rotyNew = (rotY / rotYmax + 1) * len(img) / 2
+np.floor(rotxNew,rotxNew)
+np.floor(rotyNew,rotyNew)
+rotxNew = rotxNew.astype(int)
+rotyNew = rotyNew.astype(int)
+rotatedPic = np.zeros(img.shape,dtype = 'uint8')
+np.clip(rotxNew, 0, len(img) - 1, rotxNew)
+np.clip(rotyNew, 0, len(img[0]) - 1, rotyNew)
+for i in range(len(rotxNew)):
+    for j in range(len(rotyNew)):
+        rotatedPic[rotyNew[i][j]][rotxNew[i][j]] = img[i][j]
+cv2.imshow('im',rotatedPic)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+# End of Part 2
 
+#Part 3
+repeat = 3
+
+tiledPic = np.tile(lgMapped, (repeat,1,1))
+cv2.imshow('im',tiledPic)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+xNew = np.tile(wx, (repeat,1))
+yNew = cp.deepcopy(wy)
+for i in range (1,repeat):
+    yNew = np.concatenate((yNew, wy + 2*i*np.max(wy)))
 z = xNew + 1j * yNew
-# cv2.imshow('im',tiledPic)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
-# print(tiledPic.shape)
-# rows = np.linspace(start = -1, stop = 1, num = len(tiledPic))
-# cols = np.linspace(start = -1, stop = 1, num = len(tiledPic[0]))
-# x, y = np.meshgrid(cols, rows)
-# z = x + 1j*y
+# End of Part 3
+
+# Part 4
 alpha = np.arctan(np.log(r2/r1) / (2 * np.pi))
-print(alpha)
 f = np.cos(alpha)
 rz = z * f * np.exp(1j*alpha)
 rz = np.exp(rz)
@@ -80,22 +103,20 @@ rotX = np.real(rz)
 rotY = np.imag(rz)
 rxMax = np.max(abs(rotX))
 ryMax = np.max(abs(rotY))
-xNew2 = (rotX / rxMax + 1) * 400 / 2
-yNew2 = (rotY / ryMax + 1) * 400 / 2
+xNew2 = (rotX / rxMax + 1) * len(img) / 2
+yNew2 = (rotY / ryMax + 1) * len(img[0]) / 2
 np.floor(xNew2,xNew2)
 np.floor(yNew2,yNew2)
-rotatedPic = np.zeros((400,400,3),dtype='uint8')
+finalPic = np.zeros((len(img),len(img[0]),3),dtype='uint8')
 xNew2 = xNew2.astype(int)
 yNew2 = yNew2.astype(int)
-# print(xNew2, len(tiledPic), len(tiledPic[0]), rotatedPic.shape, tiledPic.shape)
-np.clip(xNew2,0,400 - 1,xNew2)
-np.clip(yNew2,0,400 - 1,yNew2)
-# print("HHHHHHHHHHHHH")
-# print(len(xNew2), len(yNew2))
-for p in range(1200):
-    for q in range(400):
-        rotatedPic[yNew2[p][q]][xNew2[p][q]] = img[p%400][q]
+np.clip(xNew2,0,len(img) - 1,xNew2)
+np.clip(yNew2,0,len(img[0]) - 1,yNew2)
+for p in range(len(xNew2)):
+    for q in range(len(xNew2[0])):
+        finalPic[yNew2[p][q]][xNew2[p][q]] = img[p%len(img)][q]
 
-cv2.imshow('im',rotatedPic)
+cv2.imshow('im',finalPic)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+# End of Part 4
